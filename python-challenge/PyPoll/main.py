5841d896e06a18c732dbad1371ca36306d14bf5e
valar <valarjeya@gmail.com>
2019-03-05 15:11:15 -0800

    Update main.py

--- python-challenge/PyPoll/main.py
+++ python-challenge/PyPoll/main.py
@@ -33,9 +33,17 @@ with open(csvpath,'r') as csvfile:
        maxvote_winner.append(value)
     #find percentage of votes
     for n in maxvote_winner:
-      vote_percent.append(round(n/len(total_votes)*100,1))
-      
-   
+      vote_percent.append(round((n/len(total_votes)*100),3))
+
+      #zips candidates,vote_percent and maxvote_winner into tuples
+    poll_data=list(zip(total_candidates,vote_percent,maxvote_winner))
+      #from poll data we are getting winner list
+    for i in poll_data:
+      print(f'{i[0]} :{i[1]} % ({i[2]})')
+    
+    #winner
+    
+    
     print (f'{vote_percent[0]},{total_candidates[0]}') 
     print(f'Total votes :{len(total_votes)}')
     
