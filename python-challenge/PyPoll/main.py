29e373bf8a52c8dbbeefdaec0a3592d51a6e07e1
valar <valarjeya@gmail.com>
2019-03-04 17:12:44 -0800

    Update main.py

--- python-challenge/PyPoll/main.py
+++ python-challenge/PyPoll/main.py
@@ -7,7 +7,7 @@ total_candidates = []
 maxvote_winner = []
 #filepath
 csvpath=os.path.join('election_data.csv')
-def pollvotes()
+#def pollvotes()
 
 
 
@@ -15,3 +15,10 @@ def pollvotes()
 with open(csvpath,'r') as csvfile:
     csvreader=csv.reader(csvfile,delimiter=',')
     next(csvreader, None)
+#appending votes  and total candidates 
+    count=0
+    for row in csvreader:
+     total_votes.append(row[0])
+     count=count+1
+    print(count)    
+        
