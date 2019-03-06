3aa64434642e84a1d088d3a4f54bfc7706acf87b
valar <valarjeya@gmail.com>
2019-03-05 17:53:39 -0800

    Update main.py

--- python-challenge/PyPoll/main.py
+++ python-challenge/PyPoll/main.py
@@ -4,7 +4,7 @@ import os
 #creating lists
 total_votes = []
 total_candidates = []
-maxvote_winner = []
+vote_count = []
 vote_percent=[]
 #using dictionary to get each candidate name 
 poll={}
@@ -30,22 +30,38 @@ with open(csvpath,'r') as csvfile:
     #create dictionary keys and values and add them to list
     for key,value in poll.items():
        total_candidates.append(key)
-       maxvote_winner.append(value)
+       vote_count.append(value)
     #find percentage of votes
-    for n in maxvote_winner:
+    for n in vote_count:
       vote_percent.append(round((n/len(total_votes)*100),3))
 
-      #zips candidates,vote_percent and maxvote_winner into tuples
-    poll_data=list(zip(total_candidates,vote_percent,maxvote_winner))
-      #from poll data we are getting winner list
+     #zips candidates,vote_percent and vote_count into tuples
+    poll_data=list(zip(total_candidates,vote_percent,vote_count))
+     
+     
+    
+    
+      
+    #find highest vote count winner
+    #for i in vote_count:
+     # if i >  i+1 :
+      #  print (f'{vote_percent[i]},{total_candidates[i]}') 
+    winner_List=[]
+    for vote in poll_data:
+       if max(vote_count) == vote[2]:
+            winner_List.append(vote[0])
+    winner=winner_List[0]    
+    print(winner)
+    print(f'Total votes :{len(total_votes)}')
+     #from poll data we are getting winner list
     for i in poll_data:
       print(f'{i[0]} :{i[1]} % ({i[2]})')
-    
+
     #winner
     
     
-    print (f'{vote_percent[0]},{total_candidates[0]}') 
-    print(f'Total votes :{len(total_votes)}')
+    #print (f'{vote_percent[0]},{total_candidates[0]}') 
+   
     
     
         
